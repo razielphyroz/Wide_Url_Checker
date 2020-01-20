23cdbb89d14e0bf68cafdb5525cae408bf7ccec7
Raziel <henrique.sonserina@hotmail.com>
2020-01-20 08:55:49 -0300

    Implementado método para fazer requisição e buscar URLs cadastradas.

--- js/painel.js
+++ js/painel.js
@@ -1,7 +1,22 @@
+var urlsArray = [];
+
 // Checks the URL pattern
 function urlPatternCheck(urlToInsert) {
   var regexMatchUrl = new RegExp("^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$");
   return regexMatchUrl.test(urlToInsert);
+}
+
+// Update the URL's array by making a request
+function reloadUrls() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'url_manager.php', true);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      urlsArray = JSON.parse(this.response);
+    }
+  }
 }
 
 // Hides all the feedback boxes
