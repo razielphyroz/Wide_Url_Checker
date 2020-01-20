e74bb01e475b062f05acbb681dac9a551195e333
Raziel <henrique.sonserina@hotmail.com>
2020-01-20 08:47:07 -0300

    Implementação de retorno de URL's cadastradas na DB

--- url_manager.php
+++ url_manager.php
@@ -17,6 +17,18 @@
     $query_insert_url = "INSERT INTO urls (`url`) VALUES ('{$url}')";
     mysqli_query($db_connection, $query_insert_url) or die("ERROR when trying to insert a new url on DB");
 
+  } else { // This GET lists all the URLs from DB.
+
+    $query_get_urls = "SELECT * FROM urls";
+    $allUrls = mysqli_query($db_connection, $query_get_urls) or die("ERROR when trying to get urls from DB");
+
+    $urlsArray = array();
+    
+    while($row = mysqli_fetch_assoc($allUrls)) {
+      $urlsArray[] = $row;
+    }
+    
+    echo json_encode($urlsArray);
   }
 
 ?>
