b1ccdff4b6a08f9d52f2b2dd8ce7dc196ebd2328
Raziel <henrique.sonserina@hotmail.com>
2020-01-20 08:52:42 -0300

    Implementado método para checar padrão de URL

--- js/painel.js
+++ js/painel.js
@@ -1,3 +1,9 @@
+// Checks the URL pattern
+function urlPatternCheck(urlToInsert) {
+  var regexMatchUrl = new RegExp("^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$");
+  return regexMatchUrl.test(urlToInsert);
+}
+
 // Hides all the feedback boxes
 function hideFeedbacks() {
   $("#errorBox").hide();
